504875f950b3dc63c8e4c32cfbceee1cc52399b7
Nathan <Nathan>
2016-05-03 17:10:50 +0800

    First commit PROPER

--- Code/src/com/chaos/main/Game.java
+++ Code/src/com/chaos/main/Game.java
@@ -1,8 +1,12 @@
 package com.chaos.main;
 
+import com.chaos.main.graphics.Screen;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 public class Game extends Canvas implements Runnable {
     private static final long serialVersionUID = 1L;
@@ -10,14 +14,22 @@ public class Game extends Canvas implements Runnable {
     public static int width = 300;
     public static int height = width / 16 * 9;
     public static int scale = 3;
+    public static String title = "Rain";
 
     private Thread thread;
     private JFrame frame;
     private boolean running = false;
 
+    private Screen screen;
+
+    private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
     public Game() {
         Dimension size = new Dimension(width * scale, height * scale);
         setPreferredSize(size);
+
+        screen = new Screen(width, height);
 
         frame = new JFrame();
     }
@@ -38,10 +50,33 @@ public class Game extends Canvas implements Runnable {
     }
 
     public void run() {
+        long lastTime = System.nanoTime();
+        long timer = System.currentTimeMillis();
+        final double ns = 1000000000.0 / 60.0;
+        double delta = 0;
+        int frames = 0;
+        int updates = 0;
         while (running) {
-            update();
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                update();
+                updates++;
+                delta--;
+            }
             render();
+            frames++;
+
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println(updates + " ups, " + frames + " fps");
+                frame.setTitle(title + " | " + updates + " ups, " + frames + " fps");
+                updates = 0;
+                frames = 0;
+            }
         }
+        stop();
     }
 
     public void update() {
@@ -53,12 +88,26 @@ public class Game extends Canvas implements Runnable {
             createBufferStrategy(3);
             return;
         }
+
+        screen.clear();
+        screen.render();
+
+        for (int i = 0; i < pixels.length; i++) {
+            pixels[i] = screen.pixels[i];
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+        g.dispose();
+        bs.show();
     }
 
     public static void main(String[] args) {
         Game game = new Game();
         game.frame.setResizable(false);
-        game.frame.setTitle("Chaos");
+        game.frame.setTitle(game.title);
         game.frame.add(game);
         game.frame.pack();
         game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -67,5 +116,4 @@ public class Game extends Canvas implements Runnable {
 
         game.start();
     }
-
 }
