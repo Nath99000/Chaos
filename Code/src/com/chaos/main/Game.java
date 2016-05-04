d629f704dbca907d02063383d4517da08295eca8
Nath99000 <nathan.morris@iinet.net.au>
2016-05-04 12:34:07 +0800

    day 2

--- Code/src/com/chaos/main/Game.java
+++ Code/src/com/chaos/main/Game.java
@@ -12,7 +12,7 @@ import java.awt.image.DataBufferInt;
 public class Game extends Canvas implements Runnable {
     private static final long serialVersionUID = 1L;
 
-    public static int width = 300;
+    public static int width = 550;
     public static int height = width / 16 * 9;
     public static int scale = 3;
     public static String title = "Rain";
@@ -74,7 +74,6 @@ public class Game extends Canvas implements Runnable {
 
             if (System.currentTimeMillis() - timer > 1000) {
                 timer += 1000;
-                System.out.println(updates + " ups, " + frames + " fps");
                 frame.setTitle(title + " | " + updates + " ups, " + frames + " fps");
                 updates = 0;
                 frames = 0;
